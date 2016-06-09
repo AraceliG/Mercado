da233dd234a565b0007a991343b7c7b032bfd504
Lourdes Gonzalez <lourdesaye16@gmail.com>
2016-06-09 19:56:45 -0300

    busqueda no funciona

--- MercadoEnvio/WindowsFormsApplication1/ABM Usuario/BuscadorCliente.cs
+++ MercadoEnvio/WindowsFormsApplication1/ABM Usuario/BuscadorCliente.cs
@@ -35,25 +35,84 @@ namespace MercadoEnvioFRBA.ABM_Usuario
 
         private void button_buscar_Click(object sender, EventArgs e)
         {
-           // actualizarGrilla();
+            actualizarGrilla();
         }
+
+      /*  private string consulta()
+        {
+            string conslt;
+            string filtro = "";
+
+            //"UPPER(EMAIL) LIKE UPPER('%" + textBox_mail.Text + "%')"
+
+            if (textBox_nombre.Text != "" && textBox_nroDoc.Text != "" && textBox_email.Text != "" && textBox_apellido.Text != "")
+            {
+               return conslt = "SELECT * FROM NOTHING_IS_IMPOSSIBLE.USUARIO WHERE UPPER(EMAIL) LIKE UPPER('%" + textBox_email.Text + "%') AND UPPER(NOMBRE) LIKE UPPER('%" + textBox_nombre.Text + "%') AND UPPER(APELLIDO) LIKE UPPER('%" + textBox_apellido.Text + "%') AND DNI='" + textBox_nroDoc.Text + "'";
+            }
+            else
+            {
+
+                conslt = "SELECT * FROM NOTHING_IS_IMPOSSIBLE.USUARIO WHERE";
+                
+
+                if (textBox_nombre.Text != "")
+                {
+                    filtro += "UPPER(NOMBRE) LIKE UPPER('%" + textBox_nombre.Text + "%')";
+                }
+
+                if (textBox_apellido.Text != "")
+                {
+                    filtro += (filtro != "") ? " AND " : "";
+                    filtro += "UPPER(APELLIDO) LIKE UPPER('%" + textBox_apellido.Text + "%')";
+
+                }
+
+
+                if (textBox_email.Text != "")
+                {
+                    filtro += (filtro != "") ? " AND " : "";
+                    filtro += "UPPER(MAIL) LIKE UPPER('%" + textBox_email.Text + "%')";
+
+                }
+
+                if (textBox_nroDoc.Text != "")
+                {
+                    filtro += (filtro != "") ? " AND " : "";
+                    filtro += "DNI = " + textBox_nroDoc.Text + "";
+                }
+
+                if (filtro != "")
+                {
+                    return conslt += " WHERE " + filtro;
+                }
+
+                else
+                {
+                    MessageBox.Show("INGRESE UN CRITERIO DE BÚSQUEDA POR FAVOR.", "Error de criterio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    
+        */
 
         public void actualizarGrilla()
 
         {
-            SqlConnection cn = AccesoBaseDeDatos.GetConnection();
 
-            SqlCommand cmd = new SqlCommand("Select * from Cliente", cn);
-
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            // WHERE UPPER(EMAIL) LIKE UPPER('%" + textBox_email.Text + "%') AND
+           SqlCommand cm= new SqlCommand();
+            cm.Connection= AccesoBaseDeDatos.GetConnection();
+            cm.CommandType = CommandType.Text;
+            cm.CommandText = "SELECT * FROM NOTHING_IS_IMPOSSIBLE.CLIENTE WHERE NOMBRE LIKE ('%" + textBox_nombre.Text + "%') AND APELLIDO LIKE ('%" + textBox_apellido.Text + "%') AND DNI= '" + textBox_nroDoc.Text + "'";
+            cm.ExecuteNonQuery();
 
             DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cm);
 
             da.Fill(dt);
-
             dataGridView.DataSource = dt;
 
-            cn.Close();
+
         }
     }
 }
