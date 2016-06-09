91b0d50357bb024857e81d27c919a5b5d520b5f8
Lourdes Gonzalez <lourdesaye16@gmail.com>
2016-06-09 14:56:12 -0300

    buscar cliente crecion de cases

--- MercadoEnvio/WindowsFormsApplication1/ABM Usuario/BuscadorCliente.cs
+++ MercadoEnvio/WindowsFormsApplication1/ABM Usuario/BuscadorCliente.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MercadoEnvioFRBA.ConexionBaseDeDatos;
+using MercadoEnvioFRBA.ConexionBaseDeDatos.DAO;
+using MercadoEnvioFRBA.ConexionBaseDeDatos.BO;
 
 namespace MercadoEnvioFRBA.ABM_Usuario
 {
     public partial class BuscadorCliente : Form
     {
+         
 
         private void Buscador_Load(object sender, EventArgs e)
         {
@@ -21,8 +25,13 @@ namespace MercadoEnvioFRBA.ABM_Usuario
         public BuscadorCliente()
         {
             InitializeComponent();
+            listaClientes = new List<Cliente>();
+            dao = new DAOCliente();
         }
 
+        private DAOCliente dao;
+        private List<Cliente> listaClientes { get; set; }
+          
 
         private void button_buscar_Click(object sender, EventArgs e)
         {
