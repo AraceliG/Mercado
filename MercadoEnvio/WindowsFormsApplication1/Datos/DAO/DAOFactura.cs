2a4caaacb8a490a829d4423b2756c2d9abaf583c
Lourdes Gonzalez <lourdesaye16@gmail.com>
2016-07-10 00:08:48 -0300

    agregados a mano

--- MercadoEnvio/WindowsFormsApplication1/Datos/DAO/DAOFactura.cs
+++ MercadoEnvio/WindowsFormsApplication1/Datos/DAO/DAOFactura.cs
@@ -50,5 +50,93 @@ namespace MercadoEnvioFRBA.Datos.DAO
             AccesoBaseDeDatos.WriteInBase("NOTHING_IS_IMPOSSIBLE.insertarItemFactura", "SP", paramList);
             unItem.nro_item = (decimal)nro_item.Value;
         }
+
+        internal static List<Factura> cumpleConFiltros(string dni, string cuit, string numFact, string importeDesde, string importeHasta, DateTime fechaDesde, DateTime fechaHasta, bool pagoEfectivo)
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            //LAS FECHAS NO SE PASAN VACIAS
+            listaParametros.Add(new SqlParameter("@fechaDesde", fechaDesde));
+            listaParametros.Add(new SqlParameter("@fechaHasta", fechaHasta));
+
+            String base_query = "";
+            String base_query_usuario = "";
+            String base_query_factura = "";
+
+            if (!String.IsNullOrEmpty(dni) || !String.IsNullOrEmpty(cuit))
+            {
+                base_query += "SELECT  nro_factura,cod_publicacion,F.userId,fecha,total,forma_pago_desc from NOTHING_IS_IMPOSSIBLE.Factura F";
+
+            }
+            else
+            {
+                base_query += "SELECT  nro_factura,cod_publicacion,F.userId,fecha,total,forma_pago_desc from NOTHING_IS_IMPOSSIBLE.Factura F where F.NRO_FACTURA > 0";
+
+            }
+
+            if (!String.IsNullOrEmpty(dni))
+            {
+                listaParametros.Add(new SqlParameter("@dni", Convert.ToDecimal(dni)));
+                base_query_usuario += ",NOTHING_IS_IMPOSSIBLE.Cliente C where c.userId=f.userId and c.dni=@dni";
+            }
+
+            if (!String.IsNullOrEmpty(cuit))
+            {
+                listaParametros.Add(new SqlParameter("@cuit", cuit));
+                base_query_usuario += ",NOTHING_IS_IMPOSSIBLE.Empresa E where REPLACE((E.CUIT),'-','')=@cuit and e.userId=f.userId";
+            }
+
+            //LAS FECHAS NO SE PASAN VACIAS
+
+            base_query_factura += " and CONVERT(date, fecha,103) <= CONVERT(date,@fechaHasta,103)";
+            base_query_factura += "and CONVERT(date, fecha,103) >= CONVERT(date,@fechaDesde,103)";
+
+            if (importeDesde.Length != 0)
+            {
+                listaParametros.Add(new SqlParameter("@importeDesde", Convert.ToInt32(importeDesde)));
+                base_query_factura += " and TOTAL >= @importeDesde ";
+            }
+
+            if (importeHasta.Length != 0)
+            {
+                listaParametros.Add(new SqlParameter("@importeHasta", Convert.ToInt32(importeHasta)));
+                base_query_factura += " and TOTAL <= @importeHasta ";
+
+            }
+
+            if (numFact.Length != 0)
+            {
+                listaParametros.Add(new SqlParameter("@numFactura", Convert.ToInt32(numFact)));
+                base_query_factura += "  and NRO_FACTURA=@numFactura  ";
+            }
+
+            if (pagoEfectivo)
+            {
+                base_query_factura += "  and FORMA_PAGO_DESC='Efectivo' ";
+            }
+
+            SqlDataReader lector = AccesoBaseDeDatos.GetDataReader(base_query + base_query_usuario + base_query_factura, "T", listaParametros);
+
+            List<Factura> facturas = new List<Factura>();
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+
+                    Factura factura = new Factura();
+                    factura.nro_factura = (decimal)lector["nro_factura"];
+                    factura.cod_publicacion = (decimal)lector["cod_publicacion"];
+                    factura.userId = (int)(decimal)lector["userId"];
+                    factura.total = (int)(decimal)lector["total"];
+                    factura.forma_pago_desc = (String)lector["forma_pago_desc"];
+                    factura.fecha = (DateTime)lector["fecha"];
+
+                    facturas.Add(factura);
+                }
+            }
+            return facturas;
+
+        }
+
     }
+
 }
