c35f0b810b38c1003e84b57a3fe76b80034fc5aa
Lourdes Gonzalez <lourdesaye16@gmail.com>
2016-07-12 14:06:02 -0300

    le puse al dni restriccion solo numero en la creacion de cliente, me había olvidado

--- MercadoEnvio/WindowsFormsApplication1/Presentacion/ABM Usuario/CrearCliente.cs
+++ MercadoEnvio/WindowsFormsApplication1/Presentacion/ABM Usuario/CrearCliente.cs
@@ -299,7 +299,12 @@ namespace MercadoEnvioFRBA.ABM_Usuario
 
         private void textBox_nroDoc_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
         }
 
 
